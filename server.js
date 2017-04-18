44638e4babd6beaaa21e7f083ec144bed4e65e07
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-04-18 10:29:58 -0700

    commit 1: recipes request

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+//importing ShoppingList model from  diff module
+// just an interface to retrieve current state of application's shopping list
+const {ShoppingList,Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -12,16 +14,30 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
+//When server starts manual addition of items below are listed for response
 // we're going to add some items to ShoppingList
 // so there's some data to look at
 ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+//Recipe additions
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('peanut butter and jelly sandwhich', ['2 pieces bread', 'peanut butter', 'jelly']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
+  //app responses by serializing the data returned by ShoppingList
+  // note endpoint does not know how ShoppingList is implemented
   res.json(ShoppingList.get());
+});
+
+//recipe request
+app.get('/recipes', (req, res) => {
+  //app responses by serializing the data returned by ShoppingList
+  // note endpoint does not know how ShoppingList is implemented
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
